2a32308cf591de2d1a8a9fe143eaf378dfadd8e5
reaksmey@me <reaksmey.contact@gmail.com>
2025-06-05 17:41:32 +0700

    UDPATE

--- src/components/samphors/Test1.vue
+++ src/components/samphors/Test1.vue
@@ -163,19 +163,4 @@ const responsiveOptions = ref([
   },
 ])
 
-const getSeverity = (status) => {
-  switch (status) {
-    case 'INSTOCK':
-      return 'success'
-
-    case 'LOWSTOCK':
-      return 'warn'
-
-    case 'OUTOFSTOCK':
-      return 'danger'
-
-    default:
-      return null
-  }
-}
 </script>
